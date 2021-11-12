535673e471c914021c64615f044eb41d5635e59f
Axm <56112475+ax-sh@users.noreply.github.com>
2021-11-12 17:44:49 +0545

    add: base template

--- pages/base/index.tsx
+++ pages/base/index.tsx
@@ -1,22 +1,30 @@
+import { OrbitControls, Sphere } from "@react-three/drei";
 import React from "react";
 import JacketCanvas from "../../components/JacketCanvas/JacketCanvas";
 import Loader from "../../components/Loader";
 
 interface SceneProps {
-    url: string;
+  url: string;
 }
 
 const Scene = ({ url }: SceneProps) => {
-    return <group></group>;
+  return (
+    <group>
+      <Sphere>
+        <meshNormalMaterial attach="material" color="white" />
+        <OrbitControls />
+      </Sphere>
+    </group>
+  );
 };
 
 export default function Index() {
-    const url = "./jacket.glb";
-    return (
-        <JacketCanvas>
-            <React.Suspense fallback={<Loader />}>
-                <Scene url={url} />
-            </React.Suspense>
-        </JacketCanvas>
-    );
+  const url = "./jacket.glb";
+  return (
+    <JacketCanvas>
+      <React.Suspense fallback={<Loader />}>
+        <Scene url={url} />
+      </React.Suspense>
+    </JacketCanvas>
+  );
 }
