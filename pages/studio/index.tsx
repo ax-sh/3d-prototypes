46714bcd3f42aee12022e2b51f0fae525272ef5b
Axm <56112475+ax-sh@users.noreply.github.com>
2021-11-12 19:09:59 +0545

    add: studio funct

--- pages/studio/index.tsx
+++ pages/studio/index.tsx
@@ -1,4 +1,7 @@
+import { OrbitControls, Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useControls } from "leva";
+import Head from "next/head";
 import React from "react";
 import JacketCanvas from "../../components/JacketCanvas/JacketCanvas";
 import Loader from "../../components/Loader";
@@ -8,22 +11,46 @@ interface StudioProps {
 }
 
 const Studio = ({ children }: StudioProps) => {
-  return <Canvas>{children}</Canvas>;
+  const { background } = useControls({ background: "000" });
+  return (
+    <Canvas style={{ height: "100vh" }}>
+      {children}
+      <color attach="background" args={[background]} />
+    </Canvas>
+  );
 };
 
 interface SceneProps {
   url: string;
 }
 
-const Scene = ({ url }: SceneProps) => {
-  return <group></group>;
+const Scene = ({ url, ...rest }: SceneProps) => {
+  const onPointerMove = ({ point, face, object: { geometry }, ...e }: any) => {
+    const { position } = geometry.attributes;
+    const a = position.getX(face.a);
+    position.setX(face.a, a + 2);
+  };
+  return (
+    <group {...rest}>
+      <Sphere onPointerMove={onPointerMove}>
+        <meshNormalMaterial />
+      </Sphere>
+    </group>
+  );
 };
 
 export default function () {
   const url = "";
+  const ref = React.useRef();
   return (
-    <Studio>
-      <Scene url={url} />
-    </Studio>
+    <>
+      <Head>
+        <title>Studio</title>
+      </Head>
+      <Studio>
+        <Scene url={url} ref={ref} />
+        <OrbitControls />
+      </Studio>
+    </>
   );
 }
